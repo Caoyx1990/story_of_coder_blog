eea4660e7c53ce57c1f4431add4cc2087805f1f0
caoyx <caoyx1990@outlook.com>
2018-01-11 22:29:56 +0800

    增加数据库表模型

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.CharField(max_length=50)
+  createTime = models.TimeField()
+  changeTime = models.TimeField()
+  classify = models.CharField(max_length=50)
+  tag = models.CharField(max_length=50)
+  content = models.TextField()
+
+class Tags(models.Model):
+  tag = models.ManyToManyField(Post.tag)
+
+class Classify(models.Model):
+  classify = models.ForeignKey(Post.classify)
